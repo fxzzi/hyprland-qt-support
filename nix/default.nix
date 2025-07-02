863c45287dbab6c8c85196af125a5bf8276b78bb
Fazzi <faaris.ansari@proton.me>
2025-07-02 20:34:54 +0100

    nix: use builtins.path for src

--- nix/default.nix
+++ nix/default.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  nix-gitignore,
   stdenv,
   cmake,
   ninja,
@@ -8,36 +7,48 @@
   pkg-config,
   hyprlang,
   version ? "0",
-}:
-stdenv.mkDerivation {
-  pname = "hyprland-qt-support";
-  inherit version;
+}: let
+  inherit (lib.sources) cleanSource cleanSourceWith;
+  inherit (lib.strings) hasSuffix;
+in
+  stdenv.mkDerivation {
+    pname = "hyprland-qt-support";
+    inherit version;
 
-  src = nix-gitignore.gitignoreSource [] ./..;
+    src = cleanSourceWith {
+      filter = name: _type: let
+        baseName = baseNameOf (toString name);
+      in
+        ! (hasSuffix ".nix" baseName);
+      src = builtins.path {
+        path = cleanSource ../.;
+        name = "source";
+      };
+    };
 
-  nativeBuildInputs = [
-    cmake
-    ninja
-    pkg-config
-    qt6.wrapQtAppsHook
-  ];
+    nativeBuildInputs = [
+      cmake
+      ninja
+      pkg-config
+      qt6.wrapQtAppsHook
+    ];
 
-  buildInputs = [
-    qt6.qtbase
-    qt6.qtdeclarative
-    qt6.qtsvg
-    qt6.qtwayland
-    hyprlang
-  ];
+    buildInputs = [
+      qt6.qtbase
+      qt6.qtdeclarative
+      qt6.qtsvg
+      qt6.qtwayland
+      hyprlang
+    ];
 
-  cmakeFlags = [
-    (lib.cmakeFeature "INSTALL_QML_PREFIX" qt6.qtbase.qtQmlPrefix)
-  ];
+    cmakeFlags = [
+      (lib.cmakeFeature "INSTALL_QML_PREFIX" qt6.qtbase.qtQmlPrefix)
+    ];
 
-  meta = {
-    description = "hyprland-qt-support";
-    homepage = "https://github.com/hyprwm/hyprland-qt-support";
-    license = lib.licenses.bsd3;
-    platforms = lib.platforms.linux;
-  };
-}
+    meta = {
+      description = "hyprland-qt-support";
+      homepage = "https://github.com/hyprwm/hyprland-qt-support";
+      license = lib.licenses.bsd3;
+      platforms = lib.platforms.linux;
+    };
+  }
